c7b9e8172caba8133c7497ff26d709c948af0bcb
kairamkonda vijayasree <75258563+vijayashree-kairamkonda@users.noreply.github.com>
2023-11-05 12:37:35 +0530

    update: props (trending component)

--- src/components/shared/Trending.jsx
+++ src/components/shared/Trending.jsx
@@ -1,6 +1,7 @@
 import optionsIcon from "../../assets/options.svg";
+import PropTypes from "prop-types";
 
-const Trending = () => {
+const Trending = ({ topicName, noOfPosts }) => {
   return (
     <div className="flex flex-col justify-between p-4">
       <div className="text-3xl font-bold ">What&apos;s happening</div>
@@ -11,12 +12,17 @@ const Trending = () => {
             <img src={optionsIcon} />
           </div>
         </div>
-        <div>#Madurai</div>
-        <div className="text-neutral-500">2345 posts</div>
+        <div>{topicName}</div>
+        <div className="text-neutral-500">{noOfPosts} posts</div>
       </div>
       <div className="text-twitter-default">Show more</div>
     </div>
   );
+};
+
+Trending.propTypes = {
+  topicName: PropTypes.string,
+  noOfPosts: PropTypes.number,
 };
 
 export default Trending;
